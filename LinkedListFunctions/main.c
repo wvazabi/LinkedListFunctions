dac769fb050484b9776e6265551898eb825bdf85
enes <eng.eneskaya@gmail.com>
2022-06-13 15:11:40 +0300

    Initial Commit

---
+++ LinkedListFunctions/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LinkedListFunctions
+//
+//  Created by Enes Kaya on 13.06.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
